7ec3b868b1dff8a1f482a72359d962342b2e0249
Shradha Kaldate <shradha.kaldate@gmail.com>
2017-07-30 18:44:10 -0400

    adding properties to communitiy.model

--- ServiceExercise/WWCCommunities/src/app/shared/community.model.ts
+++ ServiceExercise/WWCCommunities/src/app/shared/community.model.ts
@@ -2,6 +2,8 @@ import { Person } from "app/shared/person.model";
 
 export class Community {
     public leader: Person;
+    public isCurrentUserMember:boolean = false;
+    public isSelected:boolean = false;
     constructor(public id:number,public  name:string, public details?:string){
     }
 }
