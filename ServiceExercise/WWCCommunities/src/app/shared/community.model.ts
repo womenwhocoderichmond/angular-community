457890e738610872ab04824eadf3602f5389ca6d
Shradha Kaldate <shradha.kaldate@gmail.com>
2017-07-30 22:38:37 -0400

    Some more cleanup

--- ServiceExercise/WWCCommunities/src/app/shared/community.model.ts
+++ ServiceExercise/WWCCommunities/src/app/shared/community.model.ts
@@ -1,7 +1,8 @@
 import { Person } from "app/shared/person.model";
 
 export class Community {
-    public leader: Person;
+    //public leader: Person;
+    public leaderId: number;
     public isCurrentUserMember:boolean = false;
     public isSelected:boolean = false;
     constructor(public id:number,public  name:string, public details?:string){
